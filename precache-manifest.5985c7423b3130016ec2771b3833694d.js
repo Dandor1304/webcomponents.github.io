2c4af975ed8527090bf68fb4924bb1f11c9a75f6
Daniil Doroshenko <dandoroshenko@mail.ru>
2025-10-03 18:55:28 +0000

    build6

---
+++ precache-manifest.5985c7423b3130016ec2771b3833694d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceaedc6b55f4b79dfb644c8cece55bea",
+    "url": "/webcomponents.github.io/index.html"
+  },
+  {
+    "revision": "becc441174c7fac457b7",
+    "url": "/webcomponents.github.io/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/webcomponents.github.io/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "becc441174c7fac457b7",
+    "url": "/webcomponents.github.io/static/js/main.9ab29614.chunk.js"
+  },
+  {
+    "revision": "d7e717ac6d0f5647d019",
+    "url": "/webcomponents.github.io/static/js/runtime~main.fd43110f.js"
+  }
+]);
